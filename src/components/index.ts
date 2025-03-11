3693d603eafa60f998391ae8722f5cc29ef4d068
Marcelo Ramiro <marcelo.jdeveloper@outlook.com>
2025-03-11 14:55:51 -0300

    feat: export components from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export {ListItem} from './ListItem/ListItem';
 export {Checkbox} from './Checkbox/Checkbox';
+export {FloatingButton} from './FloatingButton/FloatingButton';
+export {InputText} from './InputText/InputText';
+export {Card} from './Card/Card';
