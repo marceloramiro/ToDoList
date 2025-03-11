d0adb846c822272ed68903bbf0ebf2ec8cf2bbfb
Marcelo Ramiro <marcelo.jdeveloper@outlook.com>
2025-03-11 14:57:46 -0300

    feat: add createTodoScreen logic and styles

--- src/screens/CreateToDoScreen/CreateToDoScreen.tsx
+++ src/screens/CreateToDoScreen/CreateToDoScreen.tsx
@@ -1,9 +1,55 @@
-import {Text, View} from 'react-native';
+import {Card, InputText} from '../../components';
+import {useTodoContext} from '../../contexts/TodoContext';
+import {Button, Container, ContentWrapper, TextButton, Title} from './styles';
+import {useForm} from 'react-hook-form';
+
+interface FormData {
+  id?: number;
+  title: string;
+  description: string;
+}
 
 export const CreateToDoScreen = () => {
+  const {addTodo, todos} = useTodoContext();
+  const {setValue, handleSubmit} = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    addTodo({
+      id: (todos.length + 1).toString(),
+      title: data.title,
+      description: data.description,
+      isCompleted: false,
+    });
+  };
+
   return (
-    <View>
-      <Text>Create ToDo Screen</Text>
-    </View>
+    <Container>
+      <Card>
+        <ContentWrapper>
+          <Title>Criar nova tarefa</Title>
+          <InputText
+            label="Titulo"
+            onChangeText={text => {
+              setValue('title', text);
+            }}
+          />
+          <InputText
+            label="Descrição"
+            onChangeText={text => {
+              setValue('description', text);
+            }}
+            editable
+            multiline
+            numberOfLines={4}
+            maxLength={500}
+            height={200}
+            textAlignVertical="top"
+          />
+        </ContentWrapper>
+        <Button onPress={handleSubmit(onSubmit)}>
+          <TextButton>Criar Tarefa</TextButton>
+        </Button>
+      </Card>
+    </Container>
   );
 };
---
+++ src/screens/CreateToDoScreen/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+import Theme from '../../theme';
+
+export const Container = styled.View`
+  height: 100%;
+  padding: 20px;
+  background-color: ${Theme.colors.background};
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: ${Theme.colors.gray_20};
+  margin-bottom: 20px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background-color: ${Theme.colors.blue_10};
+  padding: 10px 20px;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+export const TextButton = styled.Text`
+  color: ${Theme.colors.gray_90};
+  font-weight: bold;
+`;
+
+export const ContentWrapper = styled.View`
+  gap: 10px;
+`;
