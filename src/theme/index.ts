84a911690cbcb3e993ce827c7b6ad6208a88fa0e
Marcelo Ramiro <marcelo.jdeveloper@outlook.com>
2025-03-11 14:54:00 -0300

    feat: add more color themes

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -7,8 +7,13 @@ export const Theme = {
     gray_30: '#6f6f6f',
     gray_40: '#8f8f8f',
     gray_50: '#b0b0b0',
+    gray_60: '#d0d0d0',
+    gray_70: '#f0f0f0',
+    gray_80: '#f8f8f8',
+    gray_90: '#fafafa',
     green_10: '#00b894',
     blue_10: '#0984e3',
+    red_10: '#d63031',
   },
 };
 
